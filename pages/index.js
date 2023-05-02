f3c707396d22ba2e8f09abab6da9a0a27dc668e2
kaylalane <73676075+kaylalane@users.noreply.github.com>
2023-05-02 17:47:41 -0400

    Deleted unused pages

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Signin() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav>
+      <nav className="p-8">
         <Link href="/" aria-label="Netflix" className="logo">
           Netflix
         </Link>
